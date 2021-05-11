cf024d8591f5c061de67b83aaf227a82e9799175
Danny Andres Barrientos Agudelo <danny.barrientos@avista.co>
2021-05-11 09:40:20 -0500

    Patron Facade se base en la implementacion que es muy compleja y la simplificando utilizando un wrapper part1

--- patronfacade.js
+++ patronfacade.js
@@ -1,25 +1,43 @@
-const hhtps = require('hhtps');
+const https = require('https');
 
-const options = {
-    hostname: 'jsonplaceholder,typicode.com',
-    path: '/users',
-    method: 'GET'
-};
 
-const req = https.request(options, res =>{
-    res.setEncoding('utf8')
-    let body = ''
-    res.on('data', d =>{
-        body += d
+//TODO Definir un constante de GEt con la Url y la implementacion de promesa asyn y await
+
+const get = url => new Promise((resolve, reject) => {
+    const comp = url.split('/');
+    const host = comp.shift();
+    const options = {
+        hostname: host,
+        path: `/${comp.join('/')}`,
+        method: 'GET'
+    };
+   
+    const req = https.request(options, res =>{
+        res.setEncoding('utf8')
+        let body = ''
+        res.on('data', d => {
+            body += d
+        });
+    
+        res.on('end', d => {
+            const parsed = JSON.parse(body)
+            resolve(parsed);
+        });
     });
-
-    res.on('end', d=>{
-        const parsed = JSON.parse(body)
-        console.log(parsed);
+    
+    req.on('error', (e) => {
+        reject(e);
     });
-});
+    
+    req.end();
+})
 
-req.on('error', (e) => {
-    console.log(e);
-});
-req.end();
+//get('jsonplaceholder.typicode.com/users')
+
+
+const main  = async () => {
+    const resultado = await get('jsonplaceholder.typicode.com/users')
+    console.log('resultado APi', resultado);
+}
+
+main()
