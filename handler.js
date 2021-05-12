f550e7a2a79d86830cc5dda80a084c9c1bee46f3
Danny Barrientos <dannybarrientosagudelo@gmail.com>
2021-05-12 14:42:24 -0500

    Implementando Patron Composicion Adapter Inyeccion

--- handler.js
+++ handler.js
@@ -1,20 +1,22 @@
 //TODO Patron Inyection
 
-module.exports = ({axios}) => ({
+
+//TODO resource funcion currie
+module.exports = resource => ({axios}) => ({
     get: async (req, res) => {
-     const { data } = await axios.get('/users')
+     const { data } = await axios.get(`/${resource}`)
      return res.send(data)
     }, 
     post: async(req, res) => {
-        const { data } = await axios.post('/users',req.body)
+        const { data } = await axios.post(`/${resource}`,req.body)
         return res.send(data)
     },
     put: async(req, res) => {
-        const { data } = await axios.put(`/users/${req.params.id}`,req.body)
+        const { data } = await axios.put(`/${resource}/${req.params.id}`,req.body)
         return res.send(data)
     },
     delete: async(req, res) => {
-        const { data } = await axios.delete(`/users/${req.params.id}`)
+        const { data } = await axios.delete(`/${resource}/${req.params.id}`)
         return res.sendStatus(204)
     }
 })
--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const express = require('express')
 const parser = require('body-parser')
 const services = require('./services')
-const handler = require('./handler')
+const handlify = require('./handler')
+
+const userHandler = handlify('users')
+const postsHandler = handlify('posts')
+
 const app = express()
 
 // parse aplication/x-www-form-urlencoded
 app.use(parser.urlencoded({extended: false }))
 
 // parse aplication/json
-app.use(parser,json)
+app.use(parser.json())
 const port = 3000
 
-app.get('/', handler(services).get)
-app.post('/', handler(services).post)
-app.put('/:id', handler(services).put)
-app.delete('/:id', handler(services).delete)
+app.get('/', userHandler(services).get)
+app.post('/', userHandler(services).post)
+app.put('/:id', userHandler(services).put)
+app.delete('/:id', userHandler(services).delete)
+app.get('/posts', postsHandler(services).get)
+app.post('/posts', postsHandler(services).post)
+app.put('/posts/:id', postsHandler(services).put)
+app.delete('/posts/:id', postsHandler(services).delete)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
